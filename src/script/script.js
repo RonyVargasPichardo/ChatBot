b4854fbaec29c2dfc1c81f3a8dc27081c4ef1f50
Rony Vargas <vargaspihardo@gmail.com>
2021-08-20 02:13:44 -0400

    intregre mi chat bot a mi pagina html

---
+++ src/script/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $('.slider1').bxSlider({
+        mode: 'fade'
+    })
+    $('.slider2').bxSlider({
+        mode: 'fade'
+    })
+    $('.slide31').bxSlider({
+        mode: 'fade'
+    })
+
+})
